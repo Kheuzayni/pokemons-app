8f91f8ef3ddda3470b31d148c3710d6eae46ddfb
unknown <bayecheikhfall249@gmail.com>
2022-12-25 02:36:41 +0000

    P1 branch template detail pokemon

--- src/app/list-pokemon/list-pokemon.component.ts
+++ src/app/list-pokemon/list-pokemon.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { POKEMONS } from '../mock-pokemon-list';
+import { Pokemon } from '../pokemon';
 
 @Component({
   selector: 'app-list-pokemon',
-  templateUrl: './list-pokemon.component.html',
-  styles: [
-  ]
+  templateUrl: './list-pokemon.component.html'
 })
 export class ListPokemonComponent {
+  pokemonList: Pokemon[] = POKEMONS;
 
 }
