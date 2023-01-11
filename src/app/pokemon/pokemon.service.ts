6b3dd0528d7e65b799c81e08df0ae3e1dbc4c733
unknown <bayecheikhfall249@gmail.com>
2023-01-11 10:24:19 +0000

    Fix route detail pokemon

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -50,7 +50,7 @@ export class PokemonService {
     }
 
     getPokemonById(pokemonId: number): Observable <Pokemon|undefined> {
-      return this.http.get<Pokemon>('api/pokemonns/${pokemonId}').pipe(
+      return this.http.get<Pokemon>(`api/pokemons/${pokemonId}`).pipe(
         tap((response) => this.log(response)),
         catchError((error) => this.handleError(error, undefined))
       )
