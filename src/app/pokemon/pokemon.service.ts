409bf06a8684e970b5930f6fdfaf64bb61dbf125
unknown <bayecheikhfall249@gmail.com>
2023-01-14 15:31:53 +0000

    rechercher à partir de 2 lettres

--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -102,6 +102,11 @@ export class PokemonService {
 
     //Recherche pokemon
     searchPokemonList(term: string) : Observable <Pokemon[]>{
+
+      if ( term.length <= 1){
+        return of([]);
+      }
+      
       return this.http.get<Pokemon[]>(`api/pokemons/?name=${term}`).pipe(
         tap((response) => this.log(response)),
         catchError((error) => this.handleError(error, null))
