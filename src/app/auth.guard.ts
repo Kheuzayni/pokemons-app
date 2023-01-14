bc9a5fb9babdb5f3584cd11bf369090bf7b5a0ee
unknown <bayecheikhfall249@gmail.com>
2023-01-14 16:23:20 +0000

    Guard: test guard avec editPokemon

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -6,10 +6,15 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+  canActivate() {
+    console.log('Le guard a bien été appelé');
     return true;
   }
+  // canActivate(
+  //   route: ActivatedRouteSnapshot,
+  //   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  //   return true;
+  // }
   
 }
